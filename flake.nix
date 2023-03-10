7344636d42ad3771127c1946791f8531c289be30
SnO2WMaN <me@sno2wman.net>
2023-03-10 21:45:06 +0900

    init

---
+++ flake.nix
@@ -0,0 +1,68 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = with inputs; [
+            devshell.overlays.default
+            rust-overlay.overlays.default
+          ];
+        };
+
+        toolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = ["rust-src"];
+        };
+
+        naersk = pkgs.callPackage inputs.naersk {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
+      in {
+        packages.default = naersk.buildPackage {
+          src = ./.;
+        };
+        apps.default = flake-utils.lib.mkApp {
+          name = "helloworld";
+          drv = self.packages.${system}.default;
+        };
+        checks.default = self.packages.${system}.default;
+
+        devShells.default = pkgs.devshell.mkShell {
+          packages =
+            [toolchain]
+            ++ (with pkgs; [
+              gcc
+              direnv
+              alejandra
+              taplo-cli
+              actionlint
+            ]);
+        };
+      }
+    );
+}
